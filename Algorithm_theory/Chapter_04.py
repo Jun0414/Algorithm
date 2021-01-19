3ac388a01bea00271cd90c39eb61aeb7beef39fe
Jun0414 <gbwj123@naver.com>
2021-01-19 14:30:15 +0900

    Implementation_algo2

--- Algorithm_theory/Chapter_04.py
+++ Algorithm_theory/Chapter_04.py
@@ -84,3 +84,164 @@ for i in range(n + 1):
                 cnt += 1
 
 print(cnt)
+
+
+
+#####################################################################
+# 실전문제 2 (왕실의 나이트 p.115)
+
+point = input()
+
+
+# 내가 작성한 답안
+cnt = 0
+
+row = int(point[1])
+col = int(ord(point[0])) - int(ord('a')) + 1
+
+if col - 2 > 0:
+    if row - 1 > 0:
+        cnt += 1
+    if row + 1 < 9:
+        cnt += 1
+if col + 2 < 9:
+    if row - 1 > 0:
+        cnt += 1
+    if row + 1 < 9:
+        cnt += 1
+if row - 2 > 0:
+    if col - 1 > 0:
+        cnt += 1
+    if col + 1 < 9:
+        cnt += 1
+if row + 2 < 9:
+    if col - 1 > 0:
+        cnt += 1
+    if col + 1 < 9:
+        cnt += 1
+
+print(cnt)
+
+
+# 모범 답안
+cnt = 0
+
+steps = [(-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (1, -2), (-1, 2), (1, 2)]
+
+for step in steps:
+    next_row = row + step[0]
+    next_col = col + step[1]
+
+    if next_row >= 1 and next_row <= 8 and next_col >= 1 and next_col <= 8:
+        cnt += 1
+
+print(cnt)
+
+
+
+#####################################################################
+# 실전문제 3 (게임 개발 p.118)
+
+n, m = map(int, input().split())
+
+
+# 내가 작성한 답안
+
+x, y, d = map(int, input().split())
+
+# 방문자 체크
+visit = [[0] * m for _ in range(n)]
+visit[x][y] = 1
+
+# n x m 행렬 만들기
+data = [[int(x) for x in input().split()] for _ in range(n)]
+
+# 방향 움직임 설정
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+cnt = 1
+turn_cnt = 0
+
+while True:
+    if d == 0:
+        d = 3
+    else:
+        d -= 1
+    nx = x + dx[d]
+    ny = y + dy[d]
+
+    if visit[nx][ny] == 0 and data[nx][ny] == 0:
+        cnt += 1
+        visit[nx][ny] = 1
+        x = nx
+        y = ny
+        turn_cnt = 0
+        continue
+    else:
+        turn_cnt += 1
+
+    if turn_cnt == 4:
+        nx = x - dx[d]
+        ny = y - dy[d]
+
+        if visit[nx][ny] == 0 and data[nx][ny] == 0:
+            x = nx
+            y = ny
+            turn_cnt = 0
+        else:
+            break
+
+print(cnt)
+
+
+# 모범 답안
+
+d = [[0] * m for _ in range(n)]
+
+x, y, direct = map(int, input().split())
+d[x][y] = 1
+
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direct
+    direct -= 1
+    if direct == -1:
+        direct = 3
+
+cnt = 1
+turn_time = 0
+
+while True:
+    turn_left()
+    nx = x + dx[direct]
+    ny = y + dy[direct]
+
+    if d[nx][ny] == 0 and data[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        cnt += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nx = x - dx[direct]
+        ny = y - dy[direct]
+
+        if d[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+
+print(cnt)
