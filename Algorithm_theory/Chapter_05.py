409763ad318ac46828a7bd4433ab0478e3cdc175
Jun0414 <gbwj123@naver.com>
2021-01-21 11:19:08 +0900

    DFS/BFS_algo1

--- Algorithm_theory/Chapter_05.py
+++ Algorithm_theory/Chapter_05.py
@@ -106,4 +106,72 @@ graph[2].append((0, 5))
 print(graph)
 
 
+#####################################################################
+# 예제 5-8 (DFS_깊이 우선 탐색 p.142)
 
+def dfs(graph, v, visited):
+    # 현재 노드 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
+
+
+
+#####################################################################
+# 예제 5-9 (BFS_너비 우선 탐색 p.14)
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+
+    # 현재 노드 방문 처리
+    visited[start] = True
+
+    # 큐가 빌때까지
+    while queue:
+        # 하나씩 원소 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
