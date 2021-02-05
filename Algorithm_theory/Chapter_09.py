d876593a7715d53535424c284fbe7b01c38f65a1
Jun0414 <gbwj123@naver.com>
2021-02-05 17:21:51 +0900

    Floyd-Warshall_algo1

--- Algorithm_theory/Chapter_09.py
+++ Algorithm_theory/Chapter_09.py
@@ -1,5 +1,5 @@
 
-# 간단한 다익스트라 알고리즘
+# 예제 9-1 (간단한 다익스트라 알고리즘 p.237)
 
 import sys
 input = sys.stdin.readline
@@ -70,7 +70,7 @@ for i in range(1, n + 1):
         print(distance[i])
 
 
-# 개선된 다익스트라 알고리즘
+# 예제 9-2 (개선된 다익스트라 알고리즘 p.248)
 import heapq
 import sys
 
@@ -145,5 +145,60 @@ for i in range(1, n + 1):
 # 5 3 1
 # 5 6 2
 
+
+
+
+#####################################################################
+# 예제 9-3 (플로이드 워셜 알고리즘 p.257)
+
+# 무한을 10억으로 초기화
+INF = int(1e9)
+
+# 노드의 개수, 간선의 개수를 입력
+n = int(input())
+m = int(input())
+
+# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보 입력
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a에서 b로 가는 비용은 c라는 의미
+    graph[a][b] = c
+
+# 플로이드 워셜 알고리즘 수행 (경유가 비용이 적을 시 갱신)
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 출력하는 반복문
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        # 도달할 수 없을 시, 무한 출력
+        if graph[a][b] == INF:
+            print('INFINITY')
+        # 도달할 수 있을 시, 거리 출력
+        else:
+            print(graph[a][b], end='\t')
+    print()
+
+# 입력 예시
+# 4
+# 7
+# 1 2 4
+# 1 4 6
+# 2 1 3
+# 2 3 7
+# 3 1 5
+# 3 4 4
+# 4 3 2
 
 
