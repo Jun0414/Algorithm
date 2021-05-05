875cc0be6a7c55f2e4ee75ecdf46fb7803a586b7
Jun0414 <gbwj123@naver.com>
2021-05-05 13:14:13 +0900

    Modify

--- BOJ/SET1/Search_Sorting/BOJ_2750.py
+++ BOJ/SET1/Search_Sorting/BOJ_2750.py
@@ -1,55 +1,68 @@
 
+# 기초 정렬
 # 문제 2750번 수 정렬하기
 # Sorting(정렬)
 
 # O(nlogn) 정렬 : 퀵 정렬(quick sort), 병합 정렬(merge sort), 힙 정렬(heap sort)
 
 
-# 병합 정렬
-def merge_sort(data):
-    # 리스트가 2개 미만일 경우 그대로 반환
-    if len(data) < 2:
-        return data
+import sys
+r = sys.stdin.readline
 
-    # 중간 지점 저장
-    mid = len(data) // 2
-    # 중간지점을 기점으로 앞리스트와 뒤리스트 리스트 안의 인덱스가 하나가 될때까지 나누기(재귀적)
-    low_data = merge_sort(data[:mid])
-    high_data = merge_sort(data[mid:])
+n = int(r())
+input_data = list(int(r()) for _ in range(n))
 
-    # 합칠 리스트 생성
-    merged_data = []
-    l = h = 0
-    # 두 리스트 중 하나라도 마지막 인덱스까지 먼저 도달하면 루프 탈출
-    while l < len(low_data) and h < len(high_data):
-        if low_data[l] < high_data[h]:
-            merged_data.append(low_data[l])
-            l += 1
-        else:
-            merged_data.append(high_data[h])
-            h += 1
-    # 나머지 뒤에 이어붙이기
-    merged_data += low_data[l:]
-    merged_data += high_data[h:]
-
-    return merged_data
+input_data.sort()
+for i in input_data:
+    print(i)
 
 
-# 몇개 입력받을 것인지, 빈 리스트 선언
-n = int(input())
-data = []
 
-# n개만큼 입력받기
-for i in range(n):
-    data.append(int(input()))
+# # 병합 정렬
+# def merge_sort(data):
+#     # 리스트가 2개 미만일 경우 그대로 반환
+#     if len(data) < 2:
+#         return data
 
-# list.sort(data)
-# data = quick_sort(data)
-data = merge_sort(data)
+#     # 중간 지점 저장
+#     mid = len(data) // 2
+#     # 중간지점을 기점으로 앞리스트와 뒤리스트 리스트 안의 인덱스가 하나가 될때까지 나누기(재귀적)
+#     low_data = merge_sort(data[:mid])
+#     high_data = merge_sort(data[mid:])
 
-# 정렬된것 출력하기
-for i in range(n):
-    print(data[i])
+#     # 합칠 리스트 생성
+#     merged_data = []
+#     l = h = 0
+#     # 두 리스트 중 하나라도 마지막 인덱스까지 먼저 도달하면 루프 탈출
+#     while l < len(low_data) and h < len(high_data):
+#         if low_data[l] < high_data[h]:
+#             merged_data.append(low_data[l])
+#             l += 1
+#         else:
+#             merged_data.append(high_data[h])
+#             h += 1
+#     # 나머지 뒤에 이어붙이기
+#     merged_data += low_data[l:]
+#     merged_data += high_data[h:]
+
+#     return merged_data
+
+
+# # 몇개 입력받을 것인지, 빈 리스트 선언
+# n = int(input())
+# data = []
+
+# # n개만큼 입력받기
+# for i in range(n):
+#     data.append(int(input()))
+
+# # list.sort(data)
+# # data = quick_sort(data)
+# data = merge_sort(data)
+
+# # 정렬된것 출력하기
+# for i in range(n):
+#     print(data[i])
 
 
 # # 퀵 정렬(기본 내장 함수: list.sort())
